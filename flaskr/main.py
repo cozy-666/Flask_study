144fc0d8c77fd9882581d8df40b0453a7ac7cd93
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-01 22:46:15 +0900

    データの登録

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,5 +1,5 @@
 from flaskr import app
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 import sqlite3
 DATABASE = 'database.db'
 
@@ -7,7 +7,7 @@ DATABASE = 'database.db'
 def index():
     con = sqlite3.connect(DATABASE)
     db_books = con.execute('SELECT * FROM books').fetchall()
-    con.close
+    con.close()
 
     books = []
     for row in db_books:
@@ -16,8 +16,22 @@ def index():
         'index.html',
         books=books
     )
+
 @app.route('/form')
 def form():
     return render_template(
         'form.html'
     )
+
+@app.route('/register', methods=['POST'])
+def register():
+    title = request.form['title']
+    price = request.form['price']
+    arrival_day = request.form['arrival_day']
+
+    con = sqlite3.connect(DATABASE)
+    con.execute('INSERT INTO books VALUES(?, ?, ?)',
+               [title, price, arrival_day])
+    con.commit()
+    con.close()
+    return redirect(url_for('index'))
