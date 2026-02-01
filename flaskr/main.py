e03afb7087e3e778dc8fb1b1964d5185a6873610
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-01 21:53:16 +0900

    HTMLの中のif文

--- flaskr/main.py
+++ flaskr/main.py
@@ -3,17 +3,7 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    books = [
-        {
-        'title': 'はらぺこあおむし',
-        'price': 1200,
-        'arrival_day': '2026年2月1日'
-        },
-        {
-        'title': 'グリとグラ',
-        'price': 950,
-        'arrival_day': '2026年2月2日'
-        },       
+    books = [      
     ]
     return render_template(
         'index.html',
