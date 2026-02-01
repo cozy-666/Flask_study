56c1ec3cdef5fe2cca5c14cf4a4c4c6f93b908f2
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-01 22:13:09 +0900

    テーブル作成SQL

--- flaskr/main.py
+++ flaskr/main.py
@@ -1,10 +1,17 @@
 from flaskr import app
 from flask import render_template
+import sqlite3
+DATABASE = 'database.db'
 
 @app.route('/')
 def index():
-    books = [      
-    ]
+    con = sqlite3.connect(DATABASE)
+    db_books = con.execute('SELECT * FROM books').fetchall()
+    con.close
+
+    books = []
+    for row in db_books:
+        books.append({'title':row[0], 'price':row[1], 'arrival_day': row[2]})
     return render_template(
         'index.html',
         books=books
