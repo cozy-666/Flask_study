a03634f928964061dbfe241a8013ec5bca1cc89c
cozy-666 <cozy.dev.tech@gmail.com>
2026-02-01 21:42:00 +0900

    テンプレートエンジン　for文

--- flaskr/main.py
+++ flaskr/main.py
@@ -3,12 +3,19 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    book = {
+    books = [
+        {
         'title': 'はらぺこあおむし',
         'price:': 1200,
         'arrival_day': '2026年2月1日'
-    }
+        },
+        {
+        'title': 'グリとグラ',
+        'price:': 950,
+        'arrival_day': '2026年2月2日'
+        },       
+    ]
     return render_template(
         'index.html',
-        book=book
+        books=books
     )
